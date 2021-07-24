3fa5ff2026c8e2d5ddc8e4f3c28e9b626244b88b
Dev Ashar <devashar13@gmail.com>
2021-07-24 17:44:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
   // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  const [lat, setLang] = useState("");
+  const [long, setLong] = useState("");
+
+  
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
+    getMyLocation()
+
+  }, []);
   // Return the App component.
+  const getMyLocation = () => {
+    const location = window.navigator && window.navigator.geolocation
+    var bodyFormData = new FormData();
+    bodyFormData.append('latitude', lat);
+    bodyFormData.append('longitude', long);
+
+    if (location) {
+      location.getCurrentPosition((position) => {
+      console.log(position.coords.latitude)
+
+        setLang(position.coords.latitude)
+          setLong(position.coords.longitude)
+
+      }, (error) => {
+        console.log(error)
+      })
+    }
+
+  }
+  
+  const sendData = async() =>{
+    var bodyFormData = new FormData();
+    bodyFormData.append('latitude', lat);
+    bodyFormData.append('longitude', long);
+    const response = await axios({
+      method: 'post',
+      url: 'https://67a2bac3ce5e.ngrok.io ',
+      data: bodyFormData,
+      
+  });
+  const x = document.getElementById("msg").innerHTML = `${response.data.dist}`
+    console.log(response.data)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>{lat}</h1>
+      <h1>{long}</h1>
+      <button onClick={sendData}>Hello</button>
+      <div id="msg"></div>
     </div>
+    
   );
 }
 
